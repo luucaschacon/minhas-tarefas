6703e3b652584855daa17a240f5ff69e95cd3f52
Lucas Chacon <lucaschacon37@gmail.com>
2026-06-17 16:50:16 -0300

    Validação no objeto de requisição

--- src/main/java/br/com/tarefas/controller/request/TarefaRequest.java
+++ src/main/java/br/com/tarefas/controller/request/TarefaRequest.java
@@ -2,16 +2,29 @@ package br.com.tarefas.controller.request;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class TarefaRequest {
 	
 	private Integer id;
 	
+	@NotBlank(message = "{tarefa.descricao.not-blank}")
+	@Size(min = 5, max = 150, message = "{tarefa.descricao.size}")
 	private String descricao;
 	
+	@FutureOrPresent(message = "{tarefa.descricao.future-or-present}")
 	private LocalDate dataEntrega;
 	
+	@NotNull(message = "{tarefa.categoria.not-null}")
+	@Min(value = 1, message = "{tarefa.categoria.min}")
 	private Integer categoriaId;
 	
+	@NotNull(message = "{tarefa.usuario.not-null}")
+	@Min(value = 1, message = "{tarefa.usuario.min}")
 	private Integer usuarioId;
 
 	public Integer getId() {
